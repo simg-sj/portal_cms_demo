f4e8db0fbcbf421f3993efe9159fce8c05e4db86
raraDesign <rara805@naver.com>
2025-06-05 15:58:54 +0900

    모바일 호환
    - 모바일 메뉴 수정
    - 보험신청 페이지 모바일호환

--- src/app/components/pageComponents/simg/insuRequest/insuRequestPage.tsx
+++ src/app/components/pageComponents/simg/insuRequest/insuRequestPage.tsx
@@ -1,98 +1,125 @@
-'use client'
-import React, {useState} from 'react';
+"use client";
+import React, { useState } from "react";
 import Step1 from "@/app/components/pageComponents/simg/insuRequest/step1-simg";
 import Step2 from "@/app/components/pageComponents/simg/insuRequest/step2-simg";
 import Step3 from "@/app/components/pageComponents/simg/insuRequest/step3";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 interface InsuRequestType {
-    bpk : number;
-    id : string;
-    balance : number;
+  bpk: number;
+  id: string;
+  balance: number;
 }
-export default function InsuRequestPage({bpk, id, balance} : InsuRequestType) {
-    //스탭
-    const [currentStep, setCurrentStep] = useState(1);
+export default function InsuRequestPage({ bpk, id, balance }: InsuRequestType) {
+  //스탭
+  const [currentStep, setCurrentStep] = useState(1);
 
-    // Form 설정
-    const { register, reset, getValues, handleSubmit, watch, formState: { errors } } = useForm({
-        defaultValues: {
-            job : 'INSURANCE',
-            gbn : 'I_REQ',
-            bpk : bpk,
-            id : id,
-            balance : balance,
-            bNumber : "",
-            carNumber: "",
-            viNumber : "",
-            contractor : "",
-            vType: "",
-            capacity: "",
-            contractCell: "",
-            contractDay : ""
-        },
-    });
+  // Form 설정
+  const {
+    register,
+    reset,
+    getValues,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      job: "INSURANCE",
+      gbn: "I_REQ",
+      bpk: bpk,
+      id: id,
+      balance: balance,
+      bNumber: "",
+      carNumber: "",
+      viNumber: "",
+      contractor: "",
+      vType: "",
+      capacity: "",
+      contractCell: "",
+      contractDay: "",
+    },
+  });
 
-    const handleNext = () => {
-        setCurrentStep(prev => Math.min(prev + 1, 3));
-    };
+  const handleNext = () => {
+    setCurrentStep((prev) => Math.min(prev + 1, 3));
+  };
 
-    const handlePrev = () => {
-        setCurrentStep(prev => Math.max(prev - 1, 1));
-    };
+  const handlePrev = () => {
+    setCurrentStep((prev) => Math.max(prev - 1, 1));
+  };
 
-    const onSubmit = async (data: any) => {
-        if(data){
-            handleNext();
-        }else {
-            return;
+  const onSubmit = async (data: any) => {
+    if (data) {
+      handleNext();
+    } else {
+      return;
+    }
+  };
+
+  const handleReset = () => {
+    setCurrentStep(1);
+    reset();
+  };
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return (
+          <Step1
+            onNext={handleNext}
+            watch={watch}
+            getValues={getValues}
+            handleSubmit={handleSubmit}
+            errors={errors}
+            register={register}
+          />
+        );
+      case 2:
+        return (
+          <Step2
+            onNext={handleNext}
+            onPrev={handlePrev}
+            getValues={getValues}
+            register={register}
+          />
+        );
+      case 3:
+        return <Step3 onReset={handleReset} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={"flex justify-center"}>
+      <div
+        className={
+          "flex min-h-[calc(100vh-50px)] w-full justify-center rounded-xl bg-white px-8 py-6"
         }
-    };
-
-
-    const handleReset = () => {
-        setCurrentStep(1);
-        reset();
-    };
-
-
-
-    const renderStep = () => {
-        switch (currentStep) {
-            case 1:
-                return <Step1 onNext={handleNext} watch={watch} getValues={getValues} handleSubmit={handleSubmit} errors={errors} register={register} />;
-            case 2:
-                return <Step2 onNext={handleNext} onPrev={handlePrev} getValues={getValues} register={register}/>;
-            case 3:
-                return <Step3 onReset={handleReset}/>;
-            default:
-                return null;
-        }
-    };
-
-    return (
-        <div className={'flex justify-center'}>
-            <div className={'px-8 py-6 bg-white rounded-xl w-full flex justify-center min-h-[calc(100vh-50px)]'}>
-                <div>
-                    <div className="flex mx-[100px] mb-[50px] text-main-light font-bold text-2xl step mt-5">
-                        <div
-                            className={`border-main-light border-2 rounded-full px-[25px] py-[15px] ${currentStep === 1 ? 'bg-main-light text-white' : ''}`}
-                        >1
-                        </div>
-                        <div className="w-[50px] h-[4px] bg-main-light mt-7"></div>
-                        <div
-                            className={`border-main-light border-2 rounded-full px-[25px] py-[15px] ${currentStep === 2 ? 'bg-main-light text-white' : ''}`}
-                        >2
-                        </div>
-                        <div className="w-[50px] h-[4px] bg-main-light mt-7"></div>
-                        <div
-                            className={`border-main-light border-2 rounded-full px-[25px] py-[15px] ${currentStep === 3 ? 'bg-main-light text-white' : ''}`}
-                        >3
-                        </div>
-                    </div>
-                    {renderStep()}
-                </div>
+      >
+        <div>
+          <div className="step mb-[50px] mt-5 flex text-2xl font-bold text-main-light">
+            <div
+              className={`rounded-full border-2 border-main-light px-[25px] py-[15px] ${currentStep === 1 ? "bg-main-light text-white" : ""}`}
+            >
+              1
             </div>
+            <div className="mt-7 h-[4px] w-[50px] bg-main-light"></div>
+            <div
+              className={`rounded-full border-2 border-main-light px-[25px] py-[15px] ${currentStep === 2 ? "bg-main-light text-white" : ""}`}
+            >
+              2
+            </div>
+            <div className="mt-7 h-[4px] w-[50px] bg-main-light"></div>
+            <div
+              className={`rounded-full border-2 border-main-light px-[25px] py-[15px] ${currentStep === 3 ? "bg-main-light text-white" : ""}`}
+            >
+              3
+            </div>
+          </div>
+          {renderStep()}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
